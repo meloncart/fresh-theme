dda40d3d15df5ecbdace5b398985a31cbbf254f8
Samuel Georges <sam@daftspunk.com>
2026-04-23 18:21:55 +1000

    feat: faceted navigation

--- assets/js/controls/catalog-form.js
+++ assets/js/controls/catalog-form.js
@@ -1,6 +1,7 @@
 class CatalogForm extends oc.ControlBase {
     connect() {
         this.listen('change', '[data-filter-manufacturer]', this.onFilterProducts);
+        this.listen('change', '[data-filter-facet]', this.onFilterProducts);
         this.listen('change', '[data-filter-rating]', this.onFilterProducts);
         this.listen('change', '[data-control="price-slider"]', this.onFilterProducts);
     }
@@ -22,6 +23,18 @@ class CatalogForm extends oc.ControlBase {
         });
         if (ratings.length) {
             data.ratings = ratings;
+        }
+
+        const filters = {};
+        this.element.querySelectorAll('[data-filter-facet]:checked').forEach(function(el) {
+            const code = el.dataset.filterFacet;
+            if (!filters[code]) {
+                filters[code] = [];
+            }
+            filters[code].push(el.value);
+        });
+        if (Object.keys(filters).length) {
+            data.filters = filters;
         }
 
         const priceMin = this.element.querySelector('[data-filter-price-min]');
